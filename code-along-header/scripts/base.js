372013a662f0cd001859f8961a0f7c2d0fe111a0
proudbhebhe <proudbhebhe02@gmail.com>
2026-09-04 21:39:59 +0200

    Built part of the landing page assignment

---
+++ code-along-header/scripts/base.js
@@ -0,0 +1,9 @@
+const navButton = document.querySelector('#nav-button');
+const navBar = document.querySelector('#nav-bar');
+
+
+navButton.addEventListener('click', () => {
+  navButton.classList.toggle('show');
+  navBar.classList.toggle('show');
+});
+
