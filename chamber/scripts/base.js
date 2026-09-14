6647b9240af8f330618097c6ee07b7636f77d801
proudbhebhe <proudbhebhe02@gmail.com>
2026-09-14 14:04:18 +0200

    did my best to ensure it up to standard

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -6,10 +6,12 @@ const list = document.querySelector("#list");
  const view = document.querySelector("#company-directory");
 grid.addEventListener('click', ()=>{
     view.classList.toggle('gridView');
+    view.classList.remove('listView');
 });
 
 list.addEventListener('click', ()=> {
     view.classList.toggle('listView');
+    view.classList.remove('gridView');
 });
 
 if (today) today.textContent = new Date().getFullYear();
@@ -43,13 +45,17 @@ async function getData() {
         let website = document.createElement("a");
         let address = document.createElement("p");
         let image = document.createElement("img");
+        let phone = document.createElement("p");
         let membership = document.createElement("p");
         let industry = document.createElement("p");
 
         name.textContent = company.company_name;
         address.innerHTML = company.company_addresses[0];
         website.textContent = company.company_website_url;
+        phone.textContent = company.company_phone_number;
         industry.innerHTML = '<span class="label">Industry: </span>'+company.industry;
+        phone.innerHTML = '<strong>Phone:</strong>'+company.company_phone_number;
+
         website.setAttribute("href", company.company_website_url)
         image.setAttribute("src", company.image_file_name);
         image.setAttribute("alt", company.company_name);
@@ -57,9 +63,13 @@ async function getData() {
 
         card.appendChild(name);
         card.appendChild(image);
-        card.appendChild(address);      
+        card.appendChild(address);
+        card.appendChild(phone);      
         card.appendChild(industry);
         card.appendChild(website);
+
+       
+
         
         document.querySelector("#company-directory").appendChild(card);
     });
